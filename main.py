9b7b3c71d4aa2dc4cc7948e1bf008d6257bfcc75
Johna <johnpeterkrause@gmail.com>
2021-01-16 23:55:47 +0200

    implementing better systems

--- main.py
+++ main.py
@@ -36,25 +36,6 @@ links = []
 os.chdir('Music')
 home_path = os.getcwd()
 
-def get_song_info(name):
-    result = sp1.search(name)
-    track = result['tracks']['items'][0]
-
-    #get artist data and display artist data
-    artist = sp1.artist(track["artists"][0]["external_urls"]["spotify"])
-
-    #get album data and display album data
-    album = sp1.album(track["album"]["external_urls"]["spotify"])
-
-    #assigning variables to replace special characters
-    album_name = album["name"]
-    artist_name = artist["name"]
-
-    album_name = re.sub(r'[^\w\s]', '', album_name) #removes all punctuation
-    artist_name = re.sub(r'[^\w\s]', '', artist_name)
-
-    return artist_name, album_name
-
 def append_video_link(name):
     print(f'Getting link for {name}')
 
@@ -67,6 +48,35 @@ def append_video_link(name):
     first_video_link = soup.find(id='video-title')
 
     links.append('https://www.youtube.com' + first_video_link['href'])
+
+def get_song_info(name):
+    result = sp1.search(name)
+    track = result['tracks']['items'][0]
+
+    #get artist data and display artist data
+    artist = sp1.artist(track["artists"][0]["external_urls"]["spotify"])
+
+    #get album data and display album data
+    album = sp1.album(track["album"]["external_urls"]["spotify"])
+
+    #assigning variables to replace special characters
+    album_name = album["name"]
+    album_genres = album['genres']
+
+    artist_name = artist["name"]
+    song_name = track['name']
+    release_date = track['album']['release_date']
+
+    album_name = re.sub(r'[^\w\s]', '', album_name) #removes all punctuation
+    artist_name = re.sub(r'[^\w\s]', '', artist_name)
+    
+    file_path = os.path.join(artist_name, album_name)
+
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+    location = file_path
+
+    return location, artist_name, album_name, song_name, release_date, album_genres
 
 
 def download_video_link(link):
@@ -88,8 +98,9 @@ print('(2) Album')
 options = int(input('> '))
 
 if options == 1:
+    print("Type 'q' and ENTER when are done.")
     while 'q' not in song_names:
-        song_names.append(input('q to finish> '))
+        song_names.append(input('> '))
     song_names.remove('q')
 
 elif options == 2:
@@ -119,9 +130,12 @@ if __name__ == '__main__':
 
     for song_name in song_names:
         append_video_link(song_name)
+    
+    browser.close()
 
-    for link in links:
-        download_video_link(link)
+    for link, song_name in zipped(links, song_names):
+        location, artist_name, album, song, year, genre = get_song_info(song_name)
+        download_video_link(link, location, artist_name, album, song, year, genre)
 
     end = time.perf_counter()
 
